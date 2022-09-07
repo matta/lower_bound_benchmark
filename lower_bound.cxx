9cb1651825aeece05478909ddc169c77894557ea
Matt Armstrong <matt@rfc20.org>
2022-09-07 13:32:32 -0700

    Add a .clang-format and run it

--- lower_bound.cxx
+++ lower_bound.cxx
@@ -1,12 +1,15 @@
-#include <random>
+#include <assert.h>
+
+#include <algorithm>
 #include <queue>
+#include <random>
 
 #include "benchmark/benchmark.h"
 
 struct Node {
-    Node* left = nullptr;
-    Node* right = nullptr;
-    long key = 0;
+  Node* left = nullptr;
+  Node* right = nullptr;
+  long key = 0;
 };
 
 #ifndef __GNUC__
@@ -19,80 +22,79 @@ struct Node {
 #endif
 
 ATTRIBUTE_NOIPA Node* LowerBound(Node* x, long i) {
-    Node* lower = nullptr;
-    while (x != nullptr) {
-        if (x->key < i) {
-            lower = x;
-            x = x->left;
-        } else {
-            x = x->right;
-        }
+  Node* lower = nullptr;
+  while (x != nullptr) {
+    if (x->key < i) {
+      lower = x;
+      x = x->left;
+    } else {
+      x = x->right;
     }
-    return lower;
+  }
+  return lower;
 }
 
 // Insert function definition.
 Node* Insert(Node* root, Node* node) {
-    if (!root) {
-        return node;
-    }
-    if (node->key > root->key) {
-        root->right = Insert(root->right, node);
-    } else {
-        root->left = Insert(root->left, node);
-    }
-    return root;
+  if (!root) {
+    return node;
+  }
+  if (node->key > root->key) {
+    root->right = Insert(root->right, node);
+  } else {
+    root->left = Insert(root->left, node);
+  }
+  return root;
 }
 
-
 struct Fixture {
-    std::vector<Node> nodes;
-    std::vector<int> keys;
-    Node* root;
+  std::vector<Node> nodes;
+  std::vector<int> keys;
+  Node* root;
 
-    ATTRIBUTE_NOIPA Fixture(int key_count) {
-        std::minstd_rand0 bitgen(42);
-        std::uniform_int_distribution<> distribution(1, 100000);
+  ATTRIBUTE_NOIPA Fixture(int key_count) {
+    std::minstd_rand0 bitgen(42);
+    std::uniform_int_distribution<> distribution(1, 100000);
 
-        nodes.resize(key_count);
-        keys.reserve(nodes.size());
-        std::queue<std::pair<int, int>> queue;
-        queue.push({1,key_count});
-        root = nullptr;
-        for (Node& node : nodes) {
-          assert(!queue.empty());
-          int low = queue.front().first;
-          int high = queue.front().second;
-          queue.pop();
-          int mid = (low + high) / 2;
+    nodes.resize(key_count);
+    keys.reserve(nodes.size());
+    std::queue<std::pair<int, int>> queue;
+    queue.push({1, key_count});
+    root = nullptr;
+    for (Node& node : nodes) {
+      assert(!queue.empty());
+      int low = queue.front().first;
+      int high = queue.front().second;
+      queue.pop();
+      int mid = (low + high) / 2;
 
-          if (low <= mid-1) {
-            queue.push({low, mid-1});
-          }
-          if (mid+1 <= high) {
-            queue.push({mid + 1, high});
-          }
+      if (low <= mid - 1) {
+        queue.push({low, mid - 1});
+      }
+      if (mid + 1 <= high) {
+        queue.push({mid + 1, high});
+      }
 
-          node.key = mid;
-          keys.push_back(node.key);
-          root = Insert(root, &node);
-        }
-        assert(keys.size() == key_count);
-        assert(queue.empty());
-
-        std::shuffle(keys.begin(), keys.end(), bitgen);
+      node.key = mid;
+      keys.push_back(node.key);
+      root = Insert(root, &node);
     }
-    ATTRIBUTE_NOIPA ~Fixture() {}
+    assert(keys.size() == key_count);
+    assert(queue.empty());
+
+    std::shuffle(keys.begin(), keys.end(), bitgen);
+  }
+  ATTRIBUTE_NOIPA ~Fixture() {}
 };
 
 void BM_LowerBound(benchmark::State& state) {
-    Fixture fixture(state.range(0));
+  Fixture fixture(state.range(0));
 
-    Node* root = fixture.root;
-    while (state.KeepRunningBatch(fixture.keys.size())) {
-        for (long key : fixture.keys) {
-            benchmark::DoNotOptimize(LowerBound(root, key));
-        }
+  Node* root = fixture.root;
+  while (state.KeepRunningBatch(fixture.keys.size())) {
+    for (long key : fixture.keys) {
+      benchmark::DoNotOptimize(LowerBound(root, key));
     }
+  }
 }
-BENCHMARK(BM_LowerBound)->Range(8,1<<20);
+BENCHMARK(BM_LowerBound)->Range(8, 1 << 20);
