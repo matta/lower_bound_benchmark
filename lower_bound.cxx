11302fecc368d6cdbd648c90566a7d52bfc53e2c
Matt Armstrong <matt@rfc20.org>
2022-09-10 16:51:28 -0700

    Checkpoint lower_bound.cxx

--- lower_bound.cxx
+++ lower_bound.cxx
@@ -1,10 +1,11 @@
-#include <assert.h>
-
-#include <algorithm>
-#include <queue>
+#include <iostream>
 #include <random>
+#include <span>
+#include <sstream>
 
 #include "benchmark/benchmark.h"
+
+namespace {
 
 struct Node {
   Node* left = nullptr;
@@ -34,67 +35,238 @@ ATTRIBUTE_NOIPA Node* LowerBound(Node* x, long i) {
   return lower;
 }
 
-// Insert function definition.
-Node* Insert(Node* root, Node* node) {
-  if (!root) {
-    return node;
-  }
-  if (node->key > root->key) {
-    root->right = Insert(root->right, node);
-  } else {
-    root->left = Insert(root->left, node);
-  }
-  return root;
+struct TreeProperties {
+  int height = 0;
+  int size = 0;
+};
+
+std::ostream& operator<<(std::ostream& os, TreeProperties& properties) {
+  return os << "height:" << properties.height << " size:" << properties.size;
 }
+
+// Return the TreeProperties (height and size) of the given tree.
+TreeProperties ComputeTreeProperties(Node* node, const long* minimum = nullptr,
+                                     const long* maximum = nullptr) {
+  if (node == nullptr) {
+    return TreeProperties();
+  }
+
+  bool error = false;
+  if (minimum != nullptr && node->key <= *minimum) {
+    std::cerr
+        << "Node " << node->key
+        << " is less than the minimum allowable key implied by its parents\n";
+    error = true;
+  } else if (maximum != nullptr && node->key >= *maximum) {
+    std::cerr << "Node " << node->key
+              << " is greater than the maximum allowable key implied by its "
+                 "parents\n";
+    error = true;
+  }
+  if (error) {
+    std::cerr << "aborting...\n";
+    std::abort();
+  }
+
+  TreeProperties left = ComputeTreeProperties(node->left, minimum, &node->key);
+  TreeProperties right =
+      ComputeTreeProperties(node->right, &node->key, maximum);
+
+  return TreeProperties{.height = 1 + std::max(left.height, right.height),
+                        .size = 1 + left.size + right.size};
+}
+
+void PrintTree(std::ostream& os, Node* node) {
+  os << '(';
+  if (node != nullptr) {
+    os << node->key;
+    if (!(node->left == nullptr && node->right == nullptr)) {
+      os << ' ';
+      PrintTree(os, node->left);
+      os << ' ';
+      PrintTree(os, node->right);
+    }
+  }
+  os << ')';
+}
+
+enum class MemoryLayout { kLevelOrdered, kRandom };
+enum class AccessPattern { kAscending, kRandom };
+
+std::ostream& operator<<(std::ostream& os, MemoryLayout layout) {
+  switch (layout) {
+    case MemoryLayout::kLevelOrdered:
+      return os << "Level";
+      break;
+    case MemoryLayout::kRandom:
+      return os << "Random";
+      break;
+  }
+  return os << "MemoryLayout(" << static_cast<int>(layout) << ')';
+}
+
+std::ostream& operator<<(std::ostream& os, AccessPattern pattern) {
+  switch (pattern) {
+    case AccessPattern::kAscending:
+      return os << "Ascending";
+      break;
+    case AccessPattern::kRandom:
+      return os << "Random";
+      break;
+  }
+  return os << "AccessPattern(" << static_cast<int>(pattern) << ')';
+}
+
+constexpr bool kDebugLog = false;
 
 struct Fixture {
   std::vector<Node> nodes;
   std::vector<int> keys;
   Node* root;
 
-  ATTRIBUTE_NOIPA Fixture(int key_count) {
-    std::minstd_rand0 bitgen(42);
-    std::uniform_int_distribution<> distribution(1, 100000);
-
+  ATTRIBUTE_NOIPA Fixture(int key_count, MemoryLayout layout,
+                          AccessPattern access_pattern) {
     nodes.resize(key_count);
-    keys.reserve(nodes.size());
-    std::queue<std::pair<int, int>> queue;
-    queue.push({1, key_count});
-    root = nullptr;
-    for (Node& node : nodes) {
-      assert(!queue.empty());
-      int low = queue.front().first;
-      int high = queue.front().second;
-      queue.pop();
-      int mid = (low + high) / 2;
+    keys = AscendingIntegers(key_count);
 
-      if (low <= mid - 1) {
-        queue.push({low, mid - 1});
-      }
-      if (mid + 1 <= high) {
-        queue.push({mid + 1, high});
-      }
-
-      node.key = mid;
-      keys.push_back(node.key);
-      root = Insert(root, &node);
+    std::vector<int> layout_map = AscendingIntegers(key_count);
+    if constexpr (kDebugLog) {
+      std::cout << "MemoryLayout is " << layout << '\n';
     }
-    assert(keys.size() == key_count);
-    assert(queue.empty());
+    if (layout == MemoryLayout::kRandom) {
+      std::shuffle(layout_map.begin(), layout_map.end(),
+                   std::minstd_rand0(17));
+    }
+    if constexpr (kDebugLog) {
+      std::cout << "layout_map:";
+      for (int index : layout_map) {
+        std::cout << ' ' << index;
+      }
+      std::cout << '\n';
+    }
 
-    std::shuffle(keys.begin(), keys.end(), bitgen);
+    int key = 0;
+    root = InsertLevelOrder(layout_map, nodes, /* index= */ 0, /* key= */ key);
+
+    constexpr int kUnroll = 32;
+    while (keys.size() < kUnroll) {
+      keys.reserve(keys.size() * 2);
+      keys.insert(keys.end(), keys.begin(), keys.end());
+    }
+    if (access_pattern == AccessPattern::kRandom) {
+      std::shuffle(keys.begin(), keys.end(), std::minstd_rand0(42));
+    }
   }
   ATTRIBUTE_NOIPA ~Fixture() {}
+
+  size_t WorkingSetBytes() {
+    return nodes.size() * sizeof(nodes[0]) + keys.size() * sizeof(keys[0]);
+  }
+
+ private:
+  static std::vector<int> AscendingIntegers(int count) {
+    std::vector<int> numbers;
+    numbers.reserve(count);
+    for (int i = 0; i < count; ++i) {
+      numbers.push_back(i);
+    }
+    return numbers;
+  }
+
+  static Node* InsertLevelOrder(const std::vector<int>& layout_map,
+                                std::span<Node> nodes, int index, int& key,
+                                int depth = 0) {
+    if constexpr (kDebugLog) {
+      auto prefix = [depth]() -> std::ostream& {
+        for (int i = 0; i < depth; ++i) {
+          std::cout << "   ";
+        }
+        return std::cout;
+      };
+      prefix() << "InsertLevelOrder(key=" << index << ")";
+    }
+    // Base case for recursion
+    if (index >= layout_map.size()) {
+      if constexpr (kDebugLog) {
+        std::cout << "-> null\n";
+      }
+      return {};
+    }
+    if constexpr (kDebugLog) {
+      std::cout << "-> layout_map[" << index << "] -> " << layout_map[index]
+                << '\n';
+    }
+    Node* root = &nodes[layout_map[index]];
+    root->left =
+        InsertLevelOrder(layout_map, nodes, 2 * index + 1, key, depth + 1);
+    root->key = key++;
+    root->right =
+        InsertLevelOrder(layout_map, nodes, 2 * index + 2, key, depth + 1);
+    return root;
+  }
 };
 
-void BM_LowerBound(benchmark::State& state) {
-  Fixture fixture(state.range(0));
+void BM_LowerBound(benchmark::State& state, MemoryLayout layout,
+                   AccessPattern access_pattern) {
+  TreeProperties expected;
+  expected.height = state.range(0);
+  expected.size = (1U << expected.height) - 1;
+  Fixture fixture(expected.size, layout, access_pattern);
+  TreeProperties actual = ComputeTreeProperties(fixture.root);
+  if (expected.height != actual.height || expected.size != actual.size) {
+    std::ostringstream os;
+    os << "tree height or size mismatch; expected " << expected
+       << " != actual " << actual << "; ";
+    PrintTree(os, fixture.root);
+    state.SkipWithError(os.str().c_str());
+    return;
+  }
 
   Node* root = fixture.root;
   while (state.KeepRunningBatch(fixture.keys.size())) {
-    for (long key : fixture.keys) {
+    for (int key : fixture.keys) {
       benchmark::DoNotOptimize(LowerBound(root, key));
     }
   }
+
+  state.counters["tree_height"] =
+      benchmark::Counter(static_cast<double>(expected.height));
+  state.counters["node_count"] =
+      benchmark::Counter(static_cast<double>(expected.size));
+  state.counters["time_per_node_traversed"] =
+      benchmark::Counter(static_cast<double>(expected.height),
+                         benchmark::Counter::kIsIterationInvariantRate |
+                             benchmark::Counter::kInvert);
+  state.counters["working_set_bytes"] = benchmark::Counter(
+      static_cast<double>(fixture.WorkingSetBytes()),
+      benchmark::Counter::kDefaults, benchmark::Counter::kIs1024);
 }
-BENCHMARK(BM_LowerBound)->Range(8, 1 << 20);
+
+void RegisterAll() {
+  for (MemoryLayout layout :
+       {MemoryLayout::kLevelOrdered, MemoryLayout::kRandom}) {
+    for (AccessPattern access :
+         {AccessPattern::kAscending, AccessPattern::kRandom}) {
+      std::ostringstream os;
+      os << "BM_LowerBound/" << layout << '/' << access;
+      auto* benchmark = benchmark::RegisterBenchmark(
+          os.str().c_str(), [layout, access](benchmark::State& state) {
+            BM_LowerBound(state, layout, access);
+          });
+      for (int height = 1; height <= 24; ++height) {
+        benchmark->Arg(height);
+      }
+    }
+  }
+}
+
+}  // namespace
+
+int main(int argc, char** argv) {
+  benchmark::Initialize(&argc, argv);
+  if (benchmark::ReportUnrecognizedArguments(argc, argv)) return 1;
+  RegisterAll();
+  benchmark::RunSpecifiedBenchmarks();
+  benchmark::Shutdown();
+  return 0;
+}
